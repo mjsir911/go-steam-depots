c30b7a1e7cb106d5c5d8a53452431d9893a150b9
Marco Sirabella <marco@sirabella.org>
2019-11-30 20:50:26 -0500

    Implement Depot somewhat fully

--- steam/Depot.go
+++ steam/Depot.go
@@ -2,24 +2,38 @@ package steam
 
 import (
 	"net/url"
+	"github.com/Jleagle/valve-data-format-go/vdf"
+	"strconv"
 )
 
 type Depot struct {
-	data interface{}
+	ID string
+	MaxSize int
+	Manifests map[string]string
 }
 
-func NewDepot(id string) (d Depot, err error) {
+// func NewDepot(id string) (d Depot, err error) {
+// 	return
+// }
+
+// for now use keyvalues, eventually want to use something more
+// encoding/json-like, with type assertions & all
+func NewDepot(kv vdf.KeyValue) (d Depot, err error) {
+	d.ID = kv.Key
+	child, _ := kv.GetChild("MaxSize")
+	d.MaxSize, _ = strconv.Atoi(child.Value)
+	child, _ = kv.GetChild("manifests")
+	d.Manifests = child.GetChildrenAsMap()
 	return
 }
 
 func (d Depot) Name() string {
-	return "1113281"
-	// return d.data.ID
+	return d.ID
 }
-// 
-// func (d Depot) Size() int64 {
-// 	return d.data.Maxsize
-// }
+
+func (d Depot) Size() int64 {
+	return int64(d.MaxSize)
+}
 
 func (d Depot) URL() url.URL {
 	// http://cache20-iad1.steamcontent.com/depot/1113281
